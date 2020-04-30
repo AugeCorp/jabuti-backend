23eb1e198868c7eec51e5176a1f12c398c64ede2
Kayque Govetri <govetrikayque@gmail.com>
2020-04-29 22:19:01 -0300

    Criaçao da primeira business

---
+++ src/business/UserBusiness.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose')
+
+const User = mongoose.model('User')
+
+
+class UserBusiness {
+  constructor(params){
+    this.email = params.email
+    this.password = params.password
+    this.name = params.name
+  }
+
+  async execute(){
+
+  }
+
+  async register(){
+    try{
+      if(await User.findOne({email: this.email})){
+        return {error: "Email already exists."}
+      }
+
+      const hash = bcrypt.hashSync(this.password, 16)
+      await User.create({name: this.name, email: this.email, password: hash})
+
+      return {created: true}
+
+    }catch(err){
+      console.log(err.message)
+      return {failed: err.message}
+    }
+  }
+
+  async getUsers(){
+    try{
+      const users = await User.find()
+      return {users, success: true}
+    }catch(err){
+      console.log(err.message)
+      return {failed: err.message}
+    }
+  }
+}
+
+module.exports = UserBusiness
