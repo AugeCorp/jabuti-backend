b7cdd3a2d296a1ac5e6870934bee2f1385dbdf21
Kayque Govetri <govetrikayque@gmail.com>
2020-06-23 22:29:10 -0300

    Mudançca url novamente

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 const { MongoDb } = require('../secure/secrets.json')
 
-const dbUrl = process.env.NODE_ENV === 'production' ? MongoDb.dbUrlProduction : MongoDb.dbUrl
-console.log(dbUrl)
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+const uri = process.env.NODE_ENV === 'production' ? MongoDb.dbUrlProduction : MongoDb.dbUrl
+mongoose.connect(uri, { useNewUrlParser: true })
 
 console.log('Mongodb connected...');
 
