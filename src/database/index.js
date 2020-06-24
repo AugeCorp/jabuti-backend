9f87e9f0d6b798e6bbbfeedb42c4ea9bf5e5b098
Kayque Govetri <govetrikayque@gmail.com>
2020-06-23 22:32:45 -0300

    mudanças na conexão

--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,9 @@ const { MongoDb } = require('../secure/secrets.json')
 
 const uri = process.env.NODE_ENV === 'production' ? MongoDb.dbUrlProduction : MongoDb.dbUrl
 mongoose.connect(uri, { useNewUrlParser: true })
+mongoose.connection.once('open', () => console.log('Good to go!')).on('error', (error) => {
+  console.warn('Warning', error)
+})
 
 console.log('Mongodb connected...');
 
