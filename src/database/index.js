4de260389b178eb2e6657abc92c5047e4f5d2b16
Kayque Govetri <govetrikayque@gmail.com>
2020-06-23 22:42:59 -0300

    Subindo modificação pra master

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose')
 const { MongoDb } = require('../secure/secrets.json')
 
-const uri = process.env.NODE_ENV === 'production' ? MongoDb.dbUrlProduction : MongoDb.dbUrl
-mongoose.connect(uri, { useNewUrlParser: true })
-mongoose.connection.once('open', () => console.log('Good to go!')).on('error', (error) => {
-  console.warn('Warning', error)
-})
+mongoose.connect(MongoDb.dbUrlProduction, { useNewUrlParser: true })
 
 console.log('Mongodb connected...');
 
