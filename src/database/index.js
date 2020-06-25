249ded0b4ac05a9aa9b9be07335a3ab57c28c06c
Kayque Govetri <govetrikayque@gmail.com>
2020-06-25 17:18:37 -0300

    Arrumando o servidor para aceitar findAndx

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const { MongoDb } = require('../secure/secrets.json')
 
-mongoose.connect(MongoDb.dbUrlProduction, { useNewUrlParser: true })
+mongoose.connect(MongoDb.dbUrlProduction, { useNewUrlParser: true, useUnifiedTopology: true })
 
 console.log('Mongodb connected...');
 
